90ed52ce44628c50de8ba9ec12d2ce50726fa815
Mowery39 <kmallory03@gmail.com>
2026-04-26 13:35:21 -0500

    Add drum sound assets (10 total) for sound selection feature

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 const advancedPanel = document.querySelector(".advanced-panel");
 const page = document.querySelector(".page");
+const drumSound = new Audio("assets/sounds/splash.mp3");
 
 advancedPanel.addEventListener("click", () => {
   page.classList.toggle("advanced-open");
+});
+
+// listen for spacebar
+document.addEventListener("keydown", (e) => {
+  if (e.code === "Space") {
+    e.preventDefault();
+
+    drumSound.currentTime = 0; // lets you spam it
+    drumSound.play();
+  }
 });
